f82de5127d18a51be9c84ff2e1ce4b6db45db999
bibu <shenminghui@pathintegral.xyz>
2026-02-06 17:27:57 +0800

    feat: overwrite builtin styles of comment footer

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,8 @@ html_theme_options = {}
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['custom.css']
+
 html_theme_options = {
     "source_repository": "https://github.com/sphinx-notes/isso/",
     "source_branch": "master",
